abf6433bc9bb60cbb0c7ea587dc622dcae2d8b22
Nguyễn Quốc Bảo An <baoandng07@gmail.com>
2024-06-02 15:39:02 +0700

    Add project files.

---
+++ Object/Account.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace Object
+{
+    public class Account
+    {
+        [Key, DatabaseGenerated(DatabaseGeneratedOption.None)]
+        public int AccountId { get; set; }
+        [Required]
+        public string UserName { get; set; }
+        [Required]
+        public string Password { get; set; }
+        public string Type { get; set; } = "Normal"; // VIP or Normal
+        public int Point { get; set; }
+        public DateTime DateRegister { get; set; }
+    }
+}
---
+++ Object/ChatDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Object
+{
+    public class ChatDbContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            
+        }
+    }
+}
